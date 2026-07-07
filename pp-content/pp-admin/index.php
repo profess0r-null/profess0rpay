cd395259c4a25c5ce6949eb881f778eb664d16b3
Sakibul Hasan Talukder <sakibulhasantalukder2006@gmail.com>
2026-07-08 02:46:35 +0600

    Update admin footer links to point to GitHub

--- pp-content/pp-admin/index.php
+++ pp-content/pp-admin/index.php
@@ -435,8 +435,8 @@
                     <div class="row text-center align-items-center flex-row-reverse">
                         <div class="col-lg-auto ms-lg-auto">
                             <ul class="list-inline list-inline-dots mb-0">
-                                <li class="list-inline-item"><a href="#" target="_blank" class="link-secondary" rel="noopener">Documentation</a></li>
-                                <li class="list-inline-item"><a href="https://github.com/professorpay" target="_blank" class="link-secondary">Modules</a></li>
+                                <li class="list-inline-item"><a href="https://github.com/profess0r-null/profess0rpay#readme" target="_blank" class="link-secondary" rel="noopener">Documentation</a></li>
+                                <li class="list-inline-item"><a href="https://github.com/profess0r-null/profess0rpay" target="_blank" class="link-secondary">Modules</a></li>
                             </ul>
                         </div>
                         <div class="col-12 col-lg-auto mt-3 mt-lg-0">
